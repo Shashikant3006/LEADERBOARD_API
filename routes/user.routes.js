787f9add712700f41fe0aa669a515db92a3c39aa
Shashikant <142254614+Shashikant3006@users.noreply.github.com>
2025-01-17 10:36:49 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,12 +5,10 @@ import verifyToken from "../middlewares/verifyToken.js";
 const router = express.Router();
 
 router.get("/get-users", getAllUser);
-router.post("/get-users-info", verifyToken, getUserWithHelpOfToken);
-router.post("/get-users-info", verifyToken, getUserWithHelpOfId);
 router.patch("/claim-points", claimPoints);
 router.get("/your-daily-history", getTodayHistory);
 router.get("/your-weekly-history", getWeeklyData);
 router.get("/your-monthly-history", getMonthlyData);
-router.post("/your-history", getUserHistory);
+router.get("/your-history", getUserHistory);
 
 export default router;
